ab6bc1556a0d2c57e5c660b49e72a1ffec856620
Austin Slater <austinslater58@gmail.com>
2023-09-28 03:10:27 -0500

    solved for base case -> if the length of arr equals 1 -> return arr

--- merge_sort/01-base_case/index.js
+++ merge_sort/01-base_case/index.js
@@ -1,11 +1,16 @@
 const mergeSort = (startArray) => {
+    const length = startArray.length;
   
-}
-
-const inputArr = [3];
-
-console.log(mergeSort(inputArr));
-
-module.exports = {
-  mergeSort
-};
+    if (length === 1) {
+      return startArray;
+    }
+  }
+  
+  const inputArr = [3];
+  
+  console.log(mergeSort(inputArr));
+  
+  module.exports = {
+    mergeSort
+  };
+  
