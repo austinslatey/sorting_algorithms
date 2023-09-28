cb55ab53210d4c69b01aa6f4c42e2a30e8883095
Austin Slater <austinslater58@gmail.com>
2023-09-28 03:25:16 -0500

    module 04 starter files

---
+++ merge_sort/04-merging/index.js
@@ -0,0 +1,26 @@
+const mergeSort = (startArray) => {
+  const length = startArray.length;
+  if (length === 1) {
+    return startArray;
+  }
+  
+  const mid = Math.floor(length / 2);
+  const leftArray = startArray.slice(0, mid);
+  const rightArray = startArray.slice(mid, length);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray))
+}
+
+const merge = (leftArray, rightArray) => {
+  const sortedArray = [];
+  
+}
+
+
+const inputArr = [3, 5, 2, 90, 4, 7];
+
+console.log(mergeSort(inputArr));
+
+module.exports = {
+  mergeSort
+};
