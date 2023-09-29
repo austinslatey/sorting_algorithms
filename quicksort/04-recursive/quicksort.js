d657f87ebe207e5cc2f352bc5ec83152aad1bf35
Austin Slater <austinslater58@gmail.com>
2023-09-28 19:38:47 -0500

    added necessary parameters and implemented the recursive call to partition

--- quicksort/04-recursive/quicksort.js
+++ quicksort/04-recursive/quicksort.js
@@ -1,8 +1,13 @@
 const swap = require('./swap');
 
-const quicksort = () => {
-
-}
+const quicksort = (array, leftBound = 0, rightBound = array.length - 1) => {
+  if (rightBound > leftBound) {
+    const pivotIndex = partition(array, leftBound, rightBound);
+    quicksort(array, leftBound, pivotIndex - 1);
+    quicksort(array, pivotIndex, rightBound);
+  }
+  return array;
+};
 
 const partition = (array, leftIndex, rightIndex) => {
   const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
