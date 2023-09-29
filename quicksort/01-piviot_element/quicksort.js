9ab2a49014a685d26a5e3e4227b41026d77a158c
Austin Slater <austinslater58@gmail.com>
2023-09-28 19:14:02 -0500

    created a piviot variable to get the avg of the sum of leftIndex and rightIndex rounded down

--- quicksort/01-piviot_element/quicksort.js
+++ quicksort/01-piviot_element/quicksort.js
@@ -9,23 +9,33 @@ const quicksort = (array, leftBound = 0, rightBound = array.length - 1) => {
   return array;
 }
 
+/* Define partition() here */
 const partition = (array, leftIndex, rightIndex) => {
-  const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
-  while (leftIndex <= rightIndex) {
-    while (array[leftIndex] < pivot) {
-      leftIndex++;
-    }
-    while (array[rightIndex] > pivot) {
-      rightIndex--;
-    }
-    if (leftIndex <= rightIndex) {
-      swap(array, leftIndex, rightIndex);
-      leftIndex++;
-      rightIndex--;
-    }
+    const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+    return pivot;
   }
-  return leftIndex;
-}
+
+// OLD PARTITION FUNC
+
+// const partition = (array, leftIndex, rightIndex) => {
+//   const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+//   while (leftIndex <= rightIndex) {
+//     while (array[leftIndex] < pivot) {
+//       leftIndex++;
+//     }
+//     while (array[rightIndex] > pivot) {
+//       rightIndex--;
+//     }
+//     if (leftIndex <= rightIndex) {
+//       swap(array, leftIndex, rightIndex);
+//       leftIndex++;
+//       rightIndex--;
+//     }
+//   }
+//   return leftIndex;
+// }
+
+
 
 module.exports = {
   quicksort, 
