b38feb8c2e3051c39ed54bdb3aac8ef09ef389f1
Austin Slater <austinslater58@gmail.com>
2023-09-28 19:26:21 -0500

    handles partitioning the array based on the pivot element

--- quicksort/02-indices/quicksort.js
+++ quicksort/02-indices/quicksort.js
@@ -1,10 +1,30 @@
 const swap = require('./swap');
 
 const partition = (array, leftIndex, rightIndex) => {
-  const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+  const pivot = Math.floor((leftIndex + rightIndex) / 2);
 
-}
+  while (leftIndex <= rightIndex) {
+    // Move leftIndex until the element at leftIndex is greater than or equal to the pivot
+    while (array[leftIndex] < array[pivot]) {
+      leftIndex++;
+    }
 
+    // Move rightIndex until the element at rightIndex is less than or equal to the pivot
+    while (array[rightIndex] > array[pivot]) {
+      rightIndex--;
+    }
+
+    // Swap the elements at leftIndex and rightIndex
+    if (leftIndex <= rightIndex) {
+      [array[leftIndex], array[rightIndex]] = [array[rightIndex], array[leftIndex]];
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+
+  // Return the last leftIndex (pivot index)
+  return leftIndex;
+};
 module.exports = {
   partition
 };
