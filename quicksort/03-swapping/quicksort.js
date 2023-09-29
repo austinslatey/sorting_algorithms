b6b7efaf051077d249dd54e5b6391a3d1bfb0ce3
Austin Slater <austinslater58@gmail.com>
2023-09-28 19:32:55 -0500

    correctly swaps elements when needed

--- quicksort/03-swapping/quicksort.js
+++ quicksort/03-swapping/quicksort.js
@@ -2,15 +2,29 @@ const swap = require('./swap');
 
 const partition = (array, leftIndex, rightIndex) => {
   const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+
   while (leftIndex <= rightIndex) {
+    // Move leftIndex until the element at leftIndex is greater than or equal to the pivot
     while (array[leftIndex] < pivot) {
       leftIndex++;
     }
+
+    // Move rightIndex until the element at rightIndex is less than or equal to the pivot
     while (array[rightIndex] > pivot) {
       rightIndex--;
     }
+
+    // Swap the elements at leftIndex and rightIndex if leftIndex is less than or equal to rightIndex
+    if (leftIndex <= rightIndex) {
+      swap(array, leftIndex, rightIndex);
+      leftIndex++;
+      rightIndex--;
+    }
   }
-}
+
+  // Return the last leftIndex (pivot index)
+  return leftIndex;
+};
 
 module.exports = {
   partition
