ad89d320c23366d635c95de781f10fdf9fa705b2
Austin Slater <austinslater58@gmail.com>
2023-09-28 19:17:31 -0500

    commiting quicksort files, previous commits document progress of files

---
+++ quicksort/02-indices/quicksort.js
@@ -0,0 +1,10 @@
+const swap = require('./swap');
+
+const partition = (array, leftIndex, rightIndex) => {
+  const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+
+}
+
+module.exports = {
+  partition
+};
